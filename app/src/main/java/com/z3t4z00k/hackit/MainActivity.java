56c26c14a9c916fffb2a7b1e8907334ff5974274
z3t4z00k <z3t4z00k@gmail.com>
2019-10-10 21:10:44 +0530

    Developed MainActivity
    Signed-off-by: z3t4z00k <z3t4z00k@gmail.com>

--- app/src/main/java/com/z3t4z00k/hackit/MainActivity.java
+++ app/src/main/java/com/z3t4z00k/hackit/MainActivity.java
@@ -1,9 +1,11 @@
 package com.z3t4z00k.hackit;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.z3t4z00k.hackit.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,8 +13,29 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String URL_LOGIN = "https://fileserver2.in/android/login.php";
+    EditText ed_username, ed_password;
+    SharedPreferences sharedPreferences;
+    public static final String MY_PREFERENCES = "MyPrefs";
+    public static final String STATUS = "status";
+    public static final String USERNAME = "username";
+    private boolean status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +52,98 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button signup = findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerActivity = new Intent(MainActivity.this, SignUp.class);
+                startActivity(registerActivity);
+            }
+        });
+
+        ed_username = findViewById(R.id.usn);
+        ed_password = findViewById(R.id.psw);
+
+        sharedPreferences = getSharedPreferences(MY_PREFERENCES, Context.MODE_PRIVATE);
+
+        status = sharedPreferences.getBoolean(STATUS, false);
+
+        if(status){
+            finish();
+            Intent intent = new Intent(MainActivity.this, data.class);
+            startActivity(intent);
+        }
+    }
+
+    public void login(View view){
+        final String username = ed_username.getText().toString();
+        final String password = ed_password.getText().toString();
+
+        if(username.isEmpty()|| password.isEmpty()){
+            Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            class Login extends AsyncTask<Void, Void, String> {
+                ProgressDialog pdLoading = new ProgressDialog(MainActivity.this);
+
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+
+                    //this method will be running on UI thread
+                    pdLoading.setMessage("\tLoading...");
+                    pdLoading.setCancelable(false);
+                    pdLoading.show();
+                }
+
+                @Override
+                protected String doInBackground(Void... voids) {
+                    //creating request handler object
+                    RequestHandler requestHandler = new RequestHandler();
+
+                    //creating request parameters
+                    HashMap<String, String> params = new HashMap<>();
+                    params.put("usn", username);
+                    params.put("psw", password);
+
+                    //returing the response
+                    return requestHandler.sendPostRequest(URL_LOGIN, params);
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                    super.onPostExecute(s);
+                    pdLoading.dismiss();
+
+                    try {
+                        //converting response to json object
+                        JSONObject obj = new JSONObject(s);
+                        //if no error in response
+                        if (!obj.getBoolean("error")) {
+                            //String username = obj.getString("usn");
+                            String username=obj.getString("usn");
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString(USERNAME, username);
+                            editor.putBoolean(STATUS, true);
+                            editor.apply();
+
+                            finish();
+                            Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(MainActivity.this, data.class);
+                            startActivity(intent);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this, "Exception: " + e, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            Login login = new Login();
+            login.execute();
+        }
     }
 
     @Override
@@ -46,8 +161,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_about) {
+            Intent about = new Intent(MainActivity.this, About.class);
+            startActivity(about);
         }
 
         return super.onOptionsItemSelected(item);
